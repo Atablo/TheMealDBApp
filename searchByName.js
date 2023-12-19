3f5a18c6fd11c102d4a50fbe5337b1f5f879da8a
PabloAtahoneroGdB <alumno.107253@iesazarquiel.es>
2023-12-19 12:51:46 +0100

    WIP in template and js

--- searchByName.js
+++ searchByName.js
@@ -1,25 +1,29 @@
-let formulario=document.getElementById("searchByName");
-let plantilla=document.getElementById("meal");
-let resultados= document.getElementById("results");
-const urlComidas="www.themealdb.com/api/json/v1/1/search.php";
-let nombreComida=document.getElementById("mealName")
+let formulario = document.getElementById("searchByName");
+let plantilla = document.getElementById("meal");
+let resultados = document.getElementById("results");
+const urlComidas = "www.themealdb.com/api/json/v1/1/search.php";
+let nombreComida = document.getElementById("mealName")
 
 
-formulario.addEventListener("submit",e=>{
+formulario.addEventListener("submit", e => {
     e.preventDefault();
-    nombreComida=nombreComida.value.trim();
+    nombreComida = nombreComida.value.trim();
     getMealsByName(nombreComida).then(
-        comidas=>{
+        comidas => {
             pintaComidas(comidas)
         }
-    ).catch(error =>{
+    ).catch(error => {
         console.log(`Error al buscar las comidas: ${error}`);
     })
 })
 
-async function getMealsByName(nombreComida){
-    const urlFetch= urlComidas + "?s="+nombreComida;
+async function getMealsByName(nombreComida) {
+    const urlFetch = urlComidas + "?s=" + nombreComida;
     const resultados = await fetch(urlFetch);
-    const json= await resultados.json();
+    const json = await resultados.json();
     return json;
+}
+
+function pintaComidas(comidas) {
+
 }
