43354702c5f895af7ab17b4ebdd5b26a09141c1a
johnnyji6 <johnnyji4091@gmail.com>
2024-01-08 18:07:10 +0100

    Cols fixed Version

--- js/dynamicweb.js
+++ js/dynamicweb.js
@@ -1,34 +1,34 @@
-document.addEventListener('scroll', function () {
-    let scrolled = window.scrollY;
-    let windowHeight = window.innerHeight;
-    let bodyHeight = document.body.clientHeight;
-    // Muestra el footer cuando estás cerca del final de la página
-    document.body.classList.toggle('scrolled-footer-visible', scrolled + windowHeight >= bodyHeight - 100);
-})
+//Footer
+document.addEventListener("scroll", function () {
+  let scrolled = window.scrollY;
+  let windowHeight = window.innerHeight;
+  let bodyHeight = document.body.clientHeight;
+  // Muestra el footer cuando estás cerca del final de la página
+  document.body.classList.toggle("scrolled-footer-visible", scrolled + windowHeight >= bodyHeight - 100);
+});
 
 /*-----------------------------------------------
 /*---------------boton scroll--------------------
 -------------------------------------------------*/
-
 window.onscroll = function () {
-    scrollFunction();
+  scrollFunction();
 };
 
 function scrollFunction() {
-    var scrollTopBtn = document.getElementById("scrollTopBtn");
+  var scrollTopBtn = document.getElementById("scrollTopBtn");
 
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollTopBtn.style.display = "block";
-    } else {
-        scrollTopBtn.style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
 }
 
 // Función para animar el scroll al top de la página
 document.getElementById("scrollTopBtn").onclick = function () {
-    scrollToTop();
+  scrollToTop();
 };
 
 function scrollToTop() {
-    document.documentElement.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
